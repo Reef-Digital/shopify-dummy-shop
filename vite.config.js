a1f7a558f7641dd571916bfe5f9837e6ae8d3c5d
Thorsten Maus <maus@pirack.com>
2025-07-16 07:00:36 +0200

    Update vite.config.js
    setting base for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), cssInjectedByJsPlugin(), tailwindcss(),],
+  base: '/shopify-dummy-shop/',
   define: {
     'process.env.NODE_ENV': JSON.stringify('dev'),
   },
