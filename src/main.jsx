c876b392c3a59f36c11ea9754859b443e8d668af
linhlp <linhlpse04693@gmail.com>
2025-09-07 13:47:08 +0700

    fix build config

--- src/main.jsx
+++ src/main.jsx
@@ -2,24 +2,5 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-
-function autoRender() {
-  let el = document.getElementById("my-react-widget");
-  console.log(el) 
-  if (!el) {
-    el = document.createElement("div");
-    el.id = "my-react-widget";
-    document.body.appendChild(el);
-  };
-  const root = createRoot(el);
-  root.render(
-      <App />
-  );
-}
-
-if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", autoRender);
-  console.log('DOMContentLoaded event added')
-} else {
-  autoRender();
-}
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import tailwindcss from "@tailwindcss/vite";
 
 /// <reference types="vite/client" />
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cssInjectedByJsPlugin(), tailwindcss()],
-  define: {
-    "process.env.NODE_ENV": JSON.stringify("dev"),
-  },
-  build: {
-    rollupOptions: {
-      input: "./src/main.jsx",
-      output: {
-        format: "iife",
-        name: "MyWidget", // Optional global name
-        entryFileNames: "my-widget.iife.js",
-      },
-    },
-    minify: false,
-  },
+  plugins: [react(), tailwindcss()],
 });
